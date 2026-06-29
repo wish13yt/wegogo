b6f058fe43e9d9c22df206e942fd720ab741730f
wish <wishyt@proton.me>
2026-06-29 15:33:37 -0500

    get posts

--- client/js/getposts.js
+++ client/js/getposts.js
@@ -1,8 +1,14 @@
 function getPosts() {
+    let posts = ''
     fetch("http://127.0.0.1:5000/api/posts", {
         method: "GET",
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
-    });
+    }) 
+    .then(res => res.json())
+    .then(data => printIt(data))
+}
+let printIt = (data) => {
+    console.log(data)
 }
