44d84deb03c634f8a3f6cefc8872e3718b84977b
wish <wishyt@proton.me>
2026-06-29 18:53:13 -0500

    refresh posts on submit

--- client/js/posts.js
+++ client/js/posts.js
@@ -1,3 +1,33 @@
+function getPosts() {
+    let posts = ''
+    let url = document.getElementById("url").value;
+    document.cookie = url;
+    console.log("Set URL cookie to " + url);
+    fetch(url + "api/posts", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(res => res.json())
+    .then(data => printIt(data))
+}
+let printIt = (data) => {
+    console.log(data)
+    let keynum = 0;
+    let postnum = 1;
+    for (const posts of Object.keys(data)) {
+        let keys = Object.keys(data);
+        let curkey = keys[keynum];
+        console.log(curkey);
+        let g = document.createElement("p");
+        document.body.appendChild(g)
+        g.id = "post" + postnum.toString();
+        document.getElementById("post" + postnum.toString()).innerText = data[posts]["name"] + ": " + data[posts]["message"];
+        keynum = keynum + 1;
+        postnum = postnum + 1;
+    };
+}
 function postPost() {
     let username = document.getElementById("username").value;
     let message = document.getElementById("message").value;
@@ -17,6 +47,7 @@ function postPost() {
         }
     });
     console.log("POSTed!");
+    getPosts()
 }
 function gregGreg() {
     let url = document.getElementById("url").value;
